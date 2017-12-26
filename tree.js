1dda502efc44cbf4162d317c396a7bc3a52edc66
maxim-borisyak <mborisyak@yandex-team.ru>
2017-12-26 17:18:44 +0300

    Tree Lights

--- tree.js
+++ tree.js
@@ -1,9 +1,8 @@
+Math.fmod = function (a,b) { return Number((a - (Math.floor(a / b) * b)).toPrecision(8)); };
+
 function Tree(canvas, numberOfLights) {
   const horizon = 3.0 / 4.0;
-  const cycle = 1000;
-  const colors = [
-    '#FFE71E', '#E8780B', '#FF012A', '#6E0BE8', '#0C90FF'
-  ]
+  const cycle = 10000;
   const ctx = canvas.getContext('2d');
   const imageWidth = 272;
   const imageHeight = 606;
@@ -15,6 +14,7 @@ function Tree(canvas, numberOfLights) {
   const treeImage = document.getElementById('tree');
   const px = []
   const py = []
+  const state = []
 
   const initLights = function() {
     var c = document.createElement('canvas');
@@ -22,26 +22,44 @@ function Tree(canvas, numberOfLights) {
     c.width = treeImage.width;
     c.height = treeImage.height;
     context.drawImage(treeImage, 0, 0);
-    var myData = context.getImageData(0, 0, treeImage.width, treeImage.height);
+    var imgData = context.getImageData(0, 0, treeImage.width, treeImage.height);
+
+    const isGreen = function(x, y) {
+      const indx = x + y * treeImage.width
+      if (indx < imgData.data.length / 4) {
+        return (imgData.data[indx * 4 + 1] > 100) && (imgData.data[indx * 4 + 3] > 0)
+      } else {
+        return false;
+      }
+    }
 
     var i = 0;
-    while(i < numLights) {
-      const x = Math.round(Math.random() * (treeImage.width - 1));
-      const y = Math.round(Math.random() * (treeImage.height - 1));
-      const indx = x + y * treeImage.width
+    var j = 0
+    while(i < numLights && j < numLights * 10) {
+      const nx = j * 23.0 / numLights
+      const ny = j * 7.0 / numLights
+      const x = Math.round(nx * (treeImage.width - 1)) % treeImage.width;
+      const y = Math.round(ny * (treeImage.height - 1)) % treeImage.height;
 
-      if ((myData.data[indx * 4 + 1] > 100) && ((myData.data[indx * 4 + 3] > 0))) {
+      if (isGreen(x, y)) {
         px.push(x * 1.0 / treeImage.width);
         py.push(y * 1.0 / treeImage.height);
-
+        state.push(Math.round(Math.random() * cycle))
         i += 1;
       }
+      j += 1;
     }
   }
 
   this.init = function() {
     this.resize()
-    initLights();
+    if (treeImage.complete || (img.naturalWidth > 0)) {
+      initLights();
+    } else {
+      treeImage.onload = function() {
+        initLights();
+      }
+    }
   }
 
   this.resize = function() {
@@ -55,8 +73,6 @@ function Tree(canvas, numberOfLights) {
 
     const now = new Date().getTime()
 
-    const offset = Math.round((now % (cycle * colors.length)) / cycle);
-
     const treeX = worldWidth / 2 - imageWidth * scale / 2
     const treeY = worldHeight * horizon - imageHeight * scale * 5.0 / 6.0
     const treeW = imageWidth * scale
@@ -66,8 +82,10 @@ function Tree(canvas, numberOfLights) {
 
     for (i = 0; i < numLights; ++i) {
       ctx.beginPath()
-      const indx = (i + offset) % colors.length;
-      ctx.fillStyle = colors[indx]
+      const indx = (state[i] + now) % cycle;
+      const t = (indx % cycle) / cycle;
+
+      ctx.fillStyle = 'hsl(' + Math.floor(t * 360) + ', 75%, 50%)'
       ctx.arc(
         treeX + px[i] * treeW,
         treeY + py[i] * treeH,
